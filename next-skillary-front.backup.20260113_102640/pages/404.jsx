fd94e2a57f4291ab41e61547e06530a8cc31461d
nmhyrhn <gbs06193@gmail.com>
2026-01-13 15:51:03 +0900

    chore: 불필요한 백업 폴더 제거

--- next-skillary-front.backup.20260113_102640/pages/404.jsx
+++
@@ -1,8 +0,0 @@
-import { AppShell } from '@/creatorhub/components/layout/AppShell'
-import { NotFoundPage } from '@/creatorhub/pages/NotFoundPage'
-
-export default function NotFound() {
-  return <AppShell>{(navigate) => <NotFoundPage onNavigate={navigate} />}</AppShell>
-}
-
-
--- next-skillary-front.backup.20260113_102640/pages/index.jsx
+++
@@ -1,8 +0,0 @@
-import { AppShell } from '@/creatorhub/components/layout/AppShell'
-import { HomePage } from '@/creatorhub/pages/HomePage'
-
-export default function IndexPage() {
-  return <AppShell>{(navigate) => <HomePage onNavigate={navigate} />}</AppShell>
-}
-
-

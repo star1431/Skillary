6dc9700c214820b567017db32c989914b89c4d77
Ju-Hyun-J <jujung967@gmail.com>
2026-01-13 09:33:41 +0900

    style: CommandLineRunner 삭제(test)

--- spring-skillary-back/src/main/java/com/example/springskillaryback/SpringSkillaryBackApplication.java
+++ spring-skillary-back/src/main/java/com/example/springskillaryback/SpringSkillaryBackApplication.java
@@ -4,12 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class SpringSkillaryBackApplication {
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final RoleRepository roleRepository;
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSkillaryBackApplication.class, args);
 	}
